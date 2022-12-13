fd5d606df213a212ba2e600e113ba5d4c5411b7e
Yari-Carelli <yaricarelli@hotmail.it>
2022-12-13 19:21:29 +0000

    Install Django Allauth

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -51,6 +51,11 @@ INSTALLED_APPS = [
     'django_filters',
     'rest_framework.authtoken',
     'dj_rest_auth',
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
+    'dj_rest_auth.registration',
     'profiles',
     'posts',
     'comments',
@@ -58,7 +63,7 @@ INSTALLED_APPS = [
     'followers',
     'events',
 ]
-
+SITE_ID = 1
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
