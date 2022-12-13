78e16b7577f0323cd77074609aff8ce9d02f359e
Yari-Carelli <yaricarelli@hotmail.it>
2022-12-13 19:02:46 +0000

    Create EventSerializer class

---
+++ events/serializers.py
@@ -0,0 +1,28 @@
+from rest_framework import serializers
+from .models import Event
+
+
+class EventSerializer(serializers.ModelSerializer):
+    """
+    Serializer for Events model.
+    """
+    owner = serializers.ReadOnlyField(source='owner.username')
+    is_owner = serializers.SerializerMethodField()
+    profile_id = serializers.ReadOnlyField(source='owner.profile.id')
+    profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
+
+    def get_is_owner(self, obj):
+        """
+        Return correct user.
+        """
+        request = self.context['request']
+        return request.user == obj.owner
+
+    class Meta:
+
+        model = Event
+        fields = [
+            'id', 'owner', 'title', 'content', 'date', 'time',
+            'city', 'country', 'price', 'event_link', 'created_at',
+            'updated_at', 'is_owner', 'profile_id','profile_image',
+        ]
