964c5f1ed89941f72d5a93688250aca09fcba823
Yari-Carelli <yaricarelli@hotmail.it>
2022-12-13 16:16:09 +0000

    Add image_filter field to Post Model

--- posts/models.py
+++ posts/models.py
@@ -7,6 +7,32 @@ class Post(models.Model):
     Post model, related to 'owner', i.e. a User instance.
     Default image set so that we can always reference image.url.
     """
+    image_filter_choices = [
+        ('none', 'None'),
+        ('appetizer', 'Appetizer'),
+        ('breakfast_&_brunch', 'Breakfast & Brunch'),
+        ('lunch', 'Lunch'),
+        ('dinner', 'Dinner'),
+        ('dessert', 'Dessert'),
+        ('main_dish', 'Main Dish'),
+        ('side_dish', 'Side Dish'),
+        ('salad', 'Salad'),
+        ('condiment', 'Condiemnt'),
+        ('soup_&_stew', 'Soup & Stew'),
+        ('fruit', 'Fruit'),
+        ('vegetable', 'Vegetable'),
+        ('legume', 'Legume'),
+        ('grain', 'Grain'),
+        ('meat', 'Meat'),
+        ('poultry', 'Poultry')
+        ('fish', 'Fish'),
+        ('seafood', 'Seafood'),
+        ('egg', 'Egg'),
+        ('mushroom', 'Mushroom'),
+        ('cheese_&_dairy', 'Cheese & Dairy'),
+        ('nuts_&_seeds', 'Nuts & Seeds'),
+        ('herbs_&_spices', 'Herbs & Spices'),
+    ]
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
@@ -14,6 +40,9 @@ class Post(models.Model):
     content = models.TextField(blank=True)
     image = models.ImageField(
         upload_to='images/', default='../default_post_wdyur2', blank=True
+    )
+    image_filter = models.CharField(
+        max_length=32, choices=image_filter_choices, default='normal'
     )
 
     class Meta:
