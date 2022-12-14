b7b564b5e31fce9219871c2e4eca28bd9f9efc02
Yari-Carelli <yaricarelli@hotmail.it>
2022-12-14 17:25:58 +0000

    Create BookList View

--- books/views.py
+++ books/views.py
@@ -1,3 +1,41 @@
-from django.shortcuts import render
+from django.db.models import Count
+from rest_framework import generics, permissions, filters
+from django_filters.rest_framework import DjangoFilterBackend
+from drf_api.permissions import IsOwnerOrReadOnly
+from .models import Book
+from .serializers import BookSerializer
 
-# Create your views here.
+
+class BookList(generics.ListCreateAPIView):
+    """
+    List books or add a book if logged in
+    The perform_create method associates the book with the logged in user.
+    """
+    serializer_class = BookSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    queryset = Book.objects.annotate(
+        likes_count=Count('likes', distinct=True),
+        comments_count=Count('comment', distinct=True)
+    ).order_by('-created_at')
+    filter_backends = [
+        filters.OrderingFilter,
+        filters.SearchFilter,
+        DjangoFilterBackend,
+    ]
+    filterset_fields = [
+        'owner__followed__owner__profile',
+        'likes__owner__profile',
+        'owner__profile',
+    ]
+    search_fields = [
+        'owner__username',
+        'title',
+    ]
+    ordering_fields = [
+        'likes_count',
+        'comments_count',
+        'likes__created_at',
+    ]
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
